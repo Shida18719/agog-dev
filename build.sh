9a140e8ddc97c8e3aef864eefccd72dafe39987d
Rashidat <rawsheedah@yahoo.com>
2025-06-10 16:05:26 +0100

    Update build script for deployment

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
-# build for render build
 #!/usr/bin/env bash
+
 set -o errexit
 pip install -r requirements.txt
 python manage.py migrate
 
+if [ -f "data_backup.json" ]; then
+    python manage.py loaddata data_backup.json
+fi
 
-
-# Load initial data if fixture file exists
-python manage.py loaddata data_backup.json
 python manage.py collectstatic --noinput
