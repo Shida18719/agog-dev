fd89c8fc6264e5551bc5d9612b9ba0765826d2be
Rashidat <rawsheedah@yahoo.com>
2025-06-10 13:19:39 +0100

    Add loading build

--- build.sh
+++ build.sh
@@ -3,4 +3,9 @@
 set -o errexit
 pip install -r requirements.txt
 python manage.py migrate
+
+
+
+# Load initial data if fixture file exists
+python manage.py loaddata data_backup.json
 python manage.py collectstatic --noinput
