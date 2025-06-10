f614688722358f3df5d1c72b1946cd0c8e74a998
Rashidat <rawsheedah@yahoo.com>
2025-06-10 19:18:10 +0100

    Add WhiteNoiseMiddleware

--- agog_developer/settings.py
+++ agog_developer/settings.py
@@ -100,6 +100,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
