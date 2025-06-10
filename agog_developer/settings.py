4801b875ceb4876e6e988e580ea4844abbc25cc2
Rashidat <rawsheedah@yahoo.com>
2025-06-11 00:44:41 +0100

    Change database to postgress

--- agog_developer/settings.py
+++ agog_developer/settings.py
@@ -137,18 +137,18 @@ WSGI_APPLICATION = 'agog_developer.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # if DEBUG:
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
         
-    }
+#     }
 # else:
 
-    # DATABASES = {
-    #     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    # }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 
 # Password validation
