2cfa1a45094baf4deb8054cafe879aef97e4c31d
Rashidat <rawsheedah@yahoo.com>
2025-06-11 16:37:56 +0100

    Changed database

--- agog_developer/settings.py
+++ agog_developer/settings.py
@@ -136,6 +136,14 @@ WSGI_APPLICATION = 'agog_developer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#             'default': {
+#                 'ENGINE': 'django.db.backends.sqlite3',
+#                 'NAME': BASE_DIR / 'db.sqlite3',
+#             }
+            
+#         }
+
 # if DEBUG:
 #     DATABASES = {
 #             'default': {
