0aad255cc5b260e7d2530ca8c502a7ec7a4c254b
Rashidat <rawsheedah@yahoo.com>
2025-06-17 00:00:25 +0100

    Add debug output to build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
-
-set -o errexit
+echo "Starting build process..."
 pip install -r requirements.txt
-python manage.py migrate
+echo "About to run collectstatic..."
+python manage.py collectstatic --noinput --verbosity=2
+echo "Build complete!"
 
-if [ -f "data_backup.json" ]; then
-    python manage.py loaddata data_backup.json
-fi
 
-python manage.py collectstatic --noinput
+
+
+
+
+
+
+# #!/usr/bin/env bash
+
+# set -o errexit
+# pip install -r requirements.txt
+# python manage.py migrate
+
+# if [ -f "data_backup.json" ]; then
+#     python manage.py loaddata data_backup.json
+# fi
+
+# python manage.py collectstatic --noinput
