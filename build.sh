af6fc53173105fee3b36abd50e2aef6dc88dc023
Rashidat <rawsheedah@yahoo.com>
2025-06-17 18:24:06 +0100

    Remove back-up from build

--- build.sh
+++ build.sh
@@ -4,8 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py migrate
 
-if [ -f "data_backup.json" ]; then
-    python manage.py loaddata data_backup.json
-fi
-
 python manage.py collectstatic --noinput
