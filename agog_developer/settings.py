6181f69ba431418caca3e65c57719d7a56dfde12
Rashidat <rawsheedah@yahoo.com>
2025-06-21 12:05:46 +0100

    Checking cloudinary storage

--- agog_developer/settings.py
+++ agog_developer/settings.py
@@ -196,11 +196,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # For production - where collected static files will be stored
-# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+    # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
