358de202e32c6f2749a12b98551b3baae2c708bd
Rashidat <rawsheedah@yahoo.com>
2025-06-22 18:39:55 +0100

    Storage error

--- agog_developer/settings.py
+++ agog_developer/settings.py
@@ -201,7 +201,7 @@ STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
    
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
     WHITENOISE_MANIFEST_STRICT = False
     
     
